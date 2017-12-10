9525c330cc01091be728d5c94b0b8ebacf4538cc
Alexandra Filbert <alexandra.filbert@informatik.hs-fulda.de>
2017-12-10 11:38:42 +0100

    SecondActivity: Styling changed

--- app/src/main/java/com/example/khalessi/pizzabestellen/ThirdActivity.java
+++ app/src/main/java/com/example/khalessi/pizzabestellen/ThirdActivity.java
@@ -1,7 +1,9 @@
 package com.example.khalessi.pizzabestellen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        Intent auftrag = getIntent();
+        Bundle extras = auftrag.getExtras();
+        String botschaft = extras.getString(MainActivity.KEY_BOTSCHAFT);
+
+        //Daten anzeigen
+        TextView tv = (TextView) findViewById(R.id.bestelltext);
+        tv.setText(botschaft);
     }
 }
